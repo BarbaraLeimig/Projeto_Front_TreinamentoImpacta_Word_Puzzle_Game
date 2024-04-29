f878fbc3471bffa6a4c673841a3f4a5ef846794e
Bárbara Leimig <barbaraleimigdev@gmail.com>
2024-04-29 11:17:47 -0300

    Delete js directory

--- js/classAnimal.js
+++
@@ -1,9 +0,0 @@
-// Classe Animal, que é base para a construção dos objetos dos animais dentro do jogo
-export class Animal {
-  constructor(img, color, word, sound) {
-    this.img = img;
-    this.color = color;
-    this.word = word;
-    this.sound = sound;
-  }
-}
